0d8b1d66f694a89d00017c3f7c4aef31bd7aae13
Sergey Misyuk <smisyuk@gmail.com>
2022-10-10 23:51:29 +0300

    task-1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
+
+const markupArr = galleryItems.map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" 
+                        alt="${item.description}"
+                        title="${item.description}"/>
+            </a>`;
+})
+
+const zoomImage = (event) => {
+    event.preventDefault();  
+}
+
+refs.gallery.insertAdjacentHTML('afterbegin', markupArr.join(''))
+
+refs.gallery.addEventListener('click', zoomImage)
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
