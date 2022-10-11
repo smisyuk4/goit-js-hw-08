2ce25c9abcbd39bbf10ef8deca29f86d470d801a
Sergey Misyuk <smisyuk@gmail.com>
2022-10-12 00:18:48 +0300

    task -2 done, player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,12 @@
 // після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
 
 
-// 1. Ознайомся з документацією бібліотеки Vimeo плеєра.
+// 1+. Ознайомся з документацією бібліотеки Vimeo плеєра.
 // https://github.com/vimeo/player.js/#vimeo-player-api
 
-// 2. Додай бібліотеку як залежність проекту через npm.
+// 2+. Додай бібліотеку як залежність проекту через npm.
 
-// 3. Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, 
+// 3+. Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, 
 // https://github.com/vimeo/player.js/#pre-existing-player
 // але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
 
@@ -19,6 +19,11 @@
 
 // 5. Зберігай час відтворення у локальне сховище. 
 // Нехай ключем для сховища буде рядок "videoplayer-current-time".
+//       localStorage.setItem("ui-theme", "light");
+//       const theme = localStorage.getItem("ui-theme");
+//       console.log(theme);
+
+
 
 // 6. Під час перезавантаження сторінки скористайся 
 // методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
@@ -29,4 +34,26 @@
 // щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
 // https://lodash.com/
 
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const player = new Player('vimeo-player');
+const lodash = _; 
+const currentTime = sessionStorage.getItem("videoplayer-current-time");;
+
+player.setCurrentTime(currentTime).then(function(seconds) {    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':           
+            break;
+
+        default:            
+            break;
+    }
+});
+
+player.on('timeupdate', lodash.throttle(
+    function (data) {
+    console.log(Math.trunc(data.seconds));
+    sessionStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
