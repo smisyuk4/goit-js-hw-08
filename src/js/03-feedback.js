8dcbfe12dfc6b5f2cce2e2b35f7d43267af09322
Sergey Misyuk <smisyuk@gmail.com>
2022-10-14 01:03:08 +0300

    done task 3, form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,79 @@
 // Напиши скрипт, який буде зберігати значення полів 
 // у локальне сховище, коли користувач щось друкує.
 
-// 1. Відстежуй на формі подію input, 
+// 1+. Відстежуй на формі подію input, 
 // і щоразу записуй у локальне сховище об'єкт з полями email і message, 
 // у яких зберігай поточні значення полів форми.
 // Нехай ключем для сховища буде рядок "feedback-form-state".
 
-// 2. Під час завантаження сторінки перевіряй стан сховища, 
+// 2+. Під час завантаження сторінки перевіряй стан сховища, 
 // і якщо там є збережені дані, заповнюй ними поля форми.
 // В іншому випадку поля повинні бути порожніми.
 
-// 3. Під час сабміту форми очищуй сховище і поля форми,
+// 3+. Під час сабміту форми очищуй сховище і поля форми,
 // а також виводь у консоль об'єкт з полями email, 
 // message та їхніми поточними значеннями.
 
-// 4. Зроби так, щоб сховище оновлювалось не частіше, 
+// 4+. Зроби так, щоб сховище оновлювалось не частіше, 
 // ніж раз на 500 мілісекунд.
 // Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
 import _ from 'lodash';
 
-
 const lodash = _; 
+const valuesForm = {};
+let formObj = localStorage.getItem('feedback-form-state');
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea'),
+}
+
+if (formObj) {
+    const dataFromStorage = JSON.parse(formObj)
+
+    writeValuesForm(dataFromStorage);
+}
+
+function writeValuesForm(dataFromStorage){
+    const {
+        email,
+        message,
+    } = dataFromStorage;
+
+    refs.email.value = email;
+    refs.message.value = message;
+
+    valuesForm.email = email;
+    valuesForm.message = message;
+}
+
+const onInputWrite = (event) => {
+    const atrName = event.target.getAttribute('name');
+
+    if (atrName === "email") {
+        valuesForm.email = event.target.value;
+    }
+    
+    if (atrName === "message") {
+        valuesForm.message = event.target.value;
+    }
+
+    formObjValues = JSON.stringify(valuesForm);
+    localStorage.setItem("feedback-form-state", formObjValues);
+}
+
+const onButtonClick = (event) => {
+    event.preventDefault(); 
+    console.log(valuesForm)
+
+    refs.email.value = '';
+    refs.message.value = '';
+    localStorage.clear();    
+}
+
+refs.form.addEventListener('input', lodash.throttle(onInputWrite, 500));
+
+refs.form.addEventListener('submit', onButtonClick)
+
